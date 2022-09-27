a43fe07f8c08e5d302c60f0f10cad8d6b6196e46
mrbarazi-97 <mrbarazi.97@gmail.com>
2022-09-27 18:58:13 +0330

    first commit

---
+++ src/server/router/index.ts
@@ -0,0 +1,10 @@
+import superjson from "superjson";
+import { createRouter } from "./context";
+import { guestbookRouter } from "./guestbook";
+
+export const appRouter = createRouter()
+  .transformer(superjson)
+  .merge("guestbook.", guestbookRouter);
+
+// export type definition of API
+export type AppRouter = typeof appRouter;
